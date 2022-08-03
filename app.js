27aea14c8352d6b3328356f94e9ef508c192cc49
SongLong <songlong2205@gmail.com>
2022-08-03 22:15:42 +0700

    Lab01_4

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ const http = require('http');
 const { brotliDecompressSync } = require('zlib');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
+    const url = req.url;
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>My First Page</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"></input><button type="submit">Send</button></form></body>'); 
+        res.write('</html>');
+        return res.end();
+    }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My First Page</title></head>');
