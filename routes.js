9555d6cb8adb79ac39654264ed255e409c2fb75c
SongLong <songlong2205@gmail.com>
2022-08-10 16:43:41 +0700

    Lab02_1

--- routes.js
+++ routes.js
@@ -12,7 +12,7 @@ const requestHandler = (req, res) => {
         res.write('</html>');
         return res.end();
     }
-    
+
     if (url === '/message' && method === 'POST') {
         const body = [];
         req.on('data', (chunk) => {
@@ -21,7 +21,7 @@ const requestHandler = (req, res) => {
         });
         return req.on('end', () => {
             const parseBody = Buffer.concat(body).toString();
-            const message = parseBody.split('=')[1];
+            const message = parseBody.split('=')[0];
             fs.writeFile('message.txt', message, err => {
                 res.statusCode = 302;
                 res.setHeader('Location', '/');
