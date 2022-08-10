d254c18b7376b53893d0563ce4b3bfcc521795c8
SongLong <songlong2205@gmail.com>
2022-08-10 18:25:35 +0700

    Lab02_5

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
-const http = require('http');
-
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use('/', (req, res, next) => {
-    console.log('This always runs!');
-    next();
-});
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/add-product', (req, res, next) => {
-    console.log('In another middleware!');
-    res.send('<h1>The "Add Product" Page</h1>');
+    res.send('<form action="/product" method="POST"><input type="text" name="title" ><button type="submit">Add Product</button></input></form>');
+});
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
-    console.log('In another middleware2!');
     res.send('<h1>Hello from Express!</h1>');
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
