50e2137ca486f2a0f87b852bb350985a84419613
SongLong <songlong2205@gmail.com>
2022-08-16 22:35:39 +0700

    Lab03_8

--- app.js
+++ app.js
@@ -6,7 +6,14 @@ const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.engine('hbs', expressHbs());
+app.engine(
+    'hbs',
+    expressHbs({
+        layoutsDir: 'views/layouts',
+        defaultLayout: 'main-layout',
+        extname: 'hbs'
+    })
+);
 app.set('view engine', 'hbs');
 app.set('views', 'views');
 
@@ -20,7 +27,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page Not Found'});
+    res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3000);
