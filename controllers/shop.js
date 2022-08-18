6e7ba5161172eb9b9f9059c98c6e5171c4e6d9c2
SongLong <songlong2205@gmail.com>
2022-08-18 22:24:40 +0700

    Lab04_12

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
@@ -38,9 +39,11 @@ exports.getCart = (req, res, next) => {
   });
 };
 
-exports.postCart = (req, res, next)=>{
+exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  console.log(prodId);
+  Product.findById(prodId, product => {
+    Cart.addProduct(prodId, product.price);
+  });
   res.redirect('/cart');
 };
 
