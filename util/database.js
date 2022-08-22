be2d79c5955f086f56a61c0bda2cbac344597747
SongLong <songlong2205@gmail.com>
2022-08-22 21:30:34 +0700

    Lab05_3

--- util/database.js
+++ util/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     database: 'node-complete',
-    password: 141100
+    password: '141100'
 });
 
 module.exports = pool.promise();
