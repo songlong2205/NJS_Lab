b762f8575df4fda19278cf8c6a274a071fc40a58
SongLong <songlong2205@gmail.com>
2022-08-23 22:23:20 +0700

    Lab05_16

--- controllers/admin.js
+++ controllers/admin.js
@@ -22,7 +22,9 @@ exports.postAddProduct = (req, res, next) => {
     .then(result => {
       // console.log(result);
       console.log('Create Product');
-    }).catch(err => {
+      res.redirect('/admin/products');
+    })
+    .catch(err => {
       console.log(err);
     });
 };
@@ -83,6 +85,13 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId);
-  res.redirect('/admin/products');
+  Product.findByPk(prodId)
+    .then(product => {
+      return product.destroy();
+    })
+    .then(result => {
+      console.log('DESTROYED PRODUCT');
+      res.redirect('/admin/products');
+    })
+    .catch(err => console.log(err));
 };
