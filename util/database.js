ccadb605f6a2c31c6821f31d86043b61373f63cf
SongLong <songlong2205@gmail.com>
2022-08-23 20:10:48 +0700

    Lab05_8

--- util/database.js
+++ util/database.js
@@ -1,10 +1,19 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: '141100'
+const sequelize = new Sequelize('node-complete', 'root', '141100', {
+    dialect: 'mysql',
+    host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
+
+// const mysql = require('mysql2');
+
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'node-complete',
+//     password: '141100'
+// });
+
+// module.exports = pool.promise();
