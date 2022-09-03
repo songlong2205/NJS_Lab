ca6169d37ddc004902d2835e357a7708574ca9da
SongLong <songlong2205@gmail.com>
2022-09-03 21:56:53 +0700

    Lab06_04

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database');
+const mongoConnect = require('./util/database').mongoConnect;
 
 const app = express();
 
@@ -23,7 +23,8 @@ app.use((req, res, next) => {
     //         req.user = user;
     //         next();
     //     })
-    //     .catch(err => console.log(err))
+    //     .catch(err => console.log(err));
+    next();
 });
 
 app.use('/admin', adminRoutes);
