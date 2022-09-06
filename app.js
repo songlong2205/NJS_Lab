b959525b122f3b8b51a92ca5e00b38f8b1adacb4
SongLong <songlong2205@gmail.com>
2022-09-06 19:51:16 +0700

    Lab07_01

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
 const User = require('./models/user');
 
 const app = express();
@@ -32,7 +32,13 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-    app.listen(3000);
-});
-
+mongoose
+    .connect(
+        'mongodb+srv://141100:141100@funixnjs101xcluster.pgo0rpy.mongodb.net/?retryWrites=true&w=majority'
+    )
+    .then(result => {
+        app.listen(3000);
+    })
+    .catch(err => {
+        console.log(err)
+    });
