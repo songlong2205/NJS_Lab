3f6e1109607e934ec977fe463cdda3d2b24433c1
SongLong <songlong2205@gmail.com>
2022-09-07 21:27:25 +0700

    Lab07_16

--- controllers/shop.js
+++ controllers/shop.js
@@ -96,6 +96,9 @@ exports.postOrder = (req, res, next) => {
       return order.save();
     })
     .then(result => {
+      return req.user.clearCart();
+    })
+    .then(() => {
       res.redirect('/orders');
     })
     .catch(err => console.log(err));
